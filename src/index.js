4edb0502d5a8b96f2770e710b64b47d2cbb5a85a
wanderson almeida <wandersonalmeida.eng@gmail.com>
2022-03-25 11:29:32 -0400

    aplicando o nodemom yarn dev

--- src/index.js
+++ src/index.js
@@ -2,7 +2,13 @@ const express = require ('express');
 
 const app = express();
 
-// localhost:5555
+/**
+ *  GET - Buscar uma informação dentro do servidor
+ *  POST - Inserir uma informação no servidor
+ *  PUT - Alterar uma informação no servidor
+ *  PATCH - Alterar uma informação no servidor
+ *  DELETE - Deletar uma informação no servidor
+ */
 
 app.get("/", (request, response) => {
   return response.json({message: "Hello World Ignite!"});
